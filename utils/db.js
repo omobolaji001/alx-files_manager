ad63e20221961feca858631dfb46a24c5064cfa8
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-06-20 00:16:02 +0100

    utils correction

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,5 @@
 import { MongoClient } from 'mongodb';
 
-
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
