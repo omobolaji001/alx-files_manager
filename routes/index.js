62a4efe023cab748c3088aeedd6b37dcc99b7e33
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-06-20 00:12:54 +0100

    UsersController

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,8 @@ import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
-
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-
 
 module.exports = router;
