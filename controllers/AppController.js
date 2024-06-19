4acf931025e39dc10e9fe2772d3ae09a66d1cf19
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-06-20 00:08:33 +0100

    UsersController

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,18 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-
 class AppController {
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive()
+      db: dbClient.isAlive(),
     });
   }
 
   static async getStats(req, res) {
     res.status(200).json({
       users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles()
+      files: await dbClient.nbFiles(),
     });
   }
 }
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,5 @@
 import sha1 from 'sha1';
-//import { ObjectID } from 'mongodb';
-//import Queue from 'bull';
 import dbClient from '../utils/db';
-//import redisClient from '../utils/redis';
-
-//const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
 class UsersController {
   static async postNew(request, response) {
@@ -24,13 +19,13 @@ class UsersController {
       const user = await users.findOne({ email });
 
       if (user) {
-        return response.status(400).json({ error: 'Already exist' });
+        response.status(400).json({ error: 'Already exist' });
       }
       const hashedPassword = sha1(password);
       const result = await users.insertOne({ email, password: hashedPassword });
-      
+
       response.status(201).json({ id: result.insertedId, email });
-    } catch(error) {
+    } catch (error) {
       console.log(error.message);
     }
   }
